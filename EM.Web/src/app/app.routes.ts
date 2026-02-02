f0f589a3c3fdd840ea825415829836dcf82eb925
Balu223 <szalai.balazs2002@gmail.com>
2026-02-02 14:32:56 +0100

    Disabled unauthorized access to routes

--- EM.Web/src/app/app.routes.ts
+++ EM.Web/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MarketplaceChartComponent } from './Features/Dashboard/Components/Marketplace-chart/marketplace-chart.component';
 import { AppComponent } from './app.component';
+import { authGuard } from './Core/Guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -14,7 +15,8 @@ export const routes: Routes = [
   },
   {
     path: 'summary',
-    component: MarketplaceChartComponent
+    component: MarketplaceChartComponent,
+    canActivate: [authGuard]
   },
   // később:
   // { path: 'login', component: AuthLoginComponent },
